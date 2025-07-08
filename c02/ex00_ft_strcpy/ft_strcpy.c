a334e12362c9eaaa8e6933b8c0f89aaf035a04f6
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2025-07-08 19:01:28 +0200

    ft_strcpy

---
+++ c02/ex00_ft_strcpy/ft_strcpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 16:21:59 by koodal            #+#    #+#             */
+/*   Updated: 2025/07/08 19:01:10 by koodal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+char *ft_strcpy(char *dest, char *src)
+{
+    int i;
+
+    i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int main(void)
+{
+    char src[] = "this is original";
+    char dest[] = "";
+    ft_strcpy(dest, src);
+    printf("%s", dest);
+    return (0);
+}
