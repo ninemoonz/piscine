5e9cbd680ff9521f9a58407201d4fc5f81d61530
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2025-07-08 11:27:14 +0200

    ft_putstr

---
+++ c01/ex05_ft_putstr/ft_putstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 10:23:41 by koodal            #+#    #+#             */
+/*   Updated: 2025/07/08 11:26:52 by koodal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+    str[i] = '\0';
+}
+
+int main(void)
+{
+    char str[] = "Hello World";
+    ft_putstr(str);
+    return (0);
+}
