836b006ce89b3bca6aab7275629a2b2dfd3e1357
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2025-07-08 11:34:25 +0200

    ft_strlen

---
+++ c01/ex06_ft_strlen/ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 11:29:51 by koodal            #+#    #+#             */
+/*   Updated: 2025/07/08 11:34:11 by koodal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
+}
+
+int main(void)
+{
+    char str[] = "Hello World";
+    printf("the string length is %i", ft_strlen(str));
+    return (0);
+}
