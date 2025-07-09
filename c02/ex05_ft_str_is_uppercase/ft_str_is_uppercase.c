b38e4c495cdac490fc82a1a0302dcde3dcf30e4b
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2025-07-09 11:37:22 +0200

    ft_str_is_lowercase & uppercase

--- c02/ex05_ft_str_is_uppercase/ft_str_is_uppercase.c
+++ c02/ex05_ft_str_is_uppercase/ft_str_is_uppercase.c
@@ -1,32 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 11:21:08 by koodal            #+#    #+#             */
-/*   Updated: 2025/07/09 11:30:24 by koodal           ###   ########.fr       */
+/*   Updated: 2025/07/09 11:36:50 by koodal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int ft_str_is_lowercase(char *str)
+int ft_str_is_uppercase(char *str)
 {
     int i;
 
-    if (str[0] == '\0')
+    if (str[0] == '\0' || str == NULL)
     {
         return (-1);
     }
     i = 0;
     while (str[i])
     {
-        if (str[i] == '\0')
-            return -1;
-        if (str[i] >= 'a' && str[i] <= 'z')
+        if (str[i] >= 'A' && str[i] <= 'Z')
         {
             i++;
         }
@@ -40,8 +38,8 @@ int ft_str_is_lowercase(char *str)
 
 int main(void)
 {
-    char lowerArr[] = "";
-    int checker = ft_str_is_lowercase(lowerArr);
+    char alphabet[] = "HELLO";
+    int checker = ft_str_is_uppercase(alphabet);
     printf("%i\n", checker);
     return (0);
 }
