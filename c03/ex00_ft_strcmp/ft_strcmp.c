64def321d2adb7dabb727afdbd3da56cd3af1db4
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2025-07-14 17:24:44 +0200

    ft_strcmp

--- c03/ex00_ft_strcmp/ft_strcmp.c
+++ c03/ex00_ft_strcmp/ft_strcmp.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -17,11 +18,21 @@ int	ft_strcmp(char *s1, char *s2)
 	return (s1[i] - s2[i]);
 }
 
+void ft_check(char *s1, char *s2)
+{
+	int ft_check;
+	int off_check;
+
+	ft_check = ft_strcmp(s1, s2);
+	off_check = strcmp(s1, s2);
+	printf("ft_strcmp result: %i\n", ft_check);
+	printf("official strcmp result: %i\n", off_check);
+}
+
 int main(void)
 {
-	char s1[] = "hello zorld";
-	char s2[] = "hello world";
-	int checker = ft_strcmp(s1, s2);
-	printf("%i", checker);
+	char s1[] = "asdlfkasdlfkasjdf";
+	char s2[] = "asdlfkasdlfkasjdf";
+	ft_check(s1, s2);
 	return (0);
 }
