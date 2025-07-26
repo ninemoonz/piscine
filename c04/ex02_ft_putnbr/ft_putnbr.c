9ba86f15deb0421a3d572d8cbe06c136a5394a18
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2025-07-26 17:35:41 +0200

    ft_putnbr

--- c04/ex02_ft_putnbr/ft_putnbr.c
+++ c04/ex02_ft_putnbr/ft_putnbr.c
@@ -6,11 +6,13 @@
 /*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 13:41:26 by koodal            #+#    #+#             */
-/*   Updated: 2025/07/21 13:49:07 by koodal           ###   ########.fr       */
+/*   Updated: 2025/07/26 17:34:58 by koodal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
 
 void ft_putchar(char c)
 {
@@ -19,19 +21,33 @@ void ft_putchar(char c)
 
 void ft_putnbr(int nb)
 {
+    if (nb == -2147483648)
+    {
+        write(1, "-2147483648", 11);
+    }
+
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        nb = -nb;
+    }
+
     if (nb >= 10)
     {
         ft_putnbr(nb / 10);
-        ft_putnbr(nb % 10);
+        ft_putchar(nb % 10 + '0');
     }
     else
     {
-        ft_putchar(nb % 10 + '0');
+        ft_putchar(nb + '0');
     }
 }
 
 int main(void)
 {
-    ft_putnbr(141);
+    ft_putnbr(-232222);
+    printf("\n");
+    printf("%i\n", INT_MAX);
+    printf("%i\n", INT_MIN);
     return (0);
 }
